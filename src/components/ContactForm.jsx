e0af7f4311f692f1480e390454b1dfebf2ffee50
Bi2er <cvdrayr@gmail.com>
2023-03-03 13:30:22 +0500

    contact form fixes

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,39 +1,44 @@
-import { en } from "@/locales/en";
-import { ru } from "@/locales/ru";
+import { en } from '@/locales/en';
+import { ru } from '@/locales/ru';
 
-export default function ContactForm({locale}) {
-    const inputClasses =
+export default function ContactForm({ locale }) {
+  const inputClasses =
     'rounded-3xl placeholder:text-black-c dark:placeholder:text-white-c bg-cold-gray py-3 px-10 placeholder:opacity-75';
-    const buttonClasses =
+  const buttonClasses =
     'rounded-3xl bg-black-c dark:bg-white-c text-white-c dark:text-black-c py-3 px-10';
-    
-    const t = locale === 'en' ? en : ru;
-    return (
+
+  const t = locale === 'en' ? en : ru;
+  const a = locale === 'en' ? '/success' : '/ru/success';
+
+  return (
     <form
-              name='contact'
-              method='POST'
-              data-netlify='true'
-              action='/success'
-              className='flex flex-col gap-5 lg:pr-20 pr-0'>
-              <input type="hidden" name="form-name" value="contact" />
-              <input
-                type='text'
-                id='name'
-                className={inputClasses}
-                placeholder={t.yourName}
-              />
-              <input
-                type='email'
-                id='email'
-                className={inputClasses}
-                placeholder={t.yourEmail}
-              />
-              <textarea
-                id='message'
-                className={`${inputClasses} resize-none`}
-                placeholder={t.yourMessage}
-              />
-              <input type='submit' value={t.submit} className={buttonClasses} />
-            </form>
-  )
+      name='contact'
+      method='POST'
+      data-netlify='true'
+      action={a}
+      className='flex flex-col gap-5 lg:pr-20 pr-0'>
+      <input type='hidden' name='form-name' value='contact' />
+      <input
+        type='text'
+        id='name'
+        name='name'
+        className={inputClasses}
+        placeholder={t.yourName}
+      />
+      <input
+        type='email'
+        id='email'
+        name='email'
+        className={inputClasses}
+        placeholder={t.yourEmail}
+      />
+      <textarea
+        id='message'
+        name='message'
+        className={`${inputClasses} resize-none`}
+        placeholder={t.yourMessage}
+      />
+      <input type='submit' value={t.submit} className={buttonClasses} />
+    </form>
+  );
 }
